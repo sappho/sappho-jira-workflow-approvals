48e2404db536c4933ef8e602c016ed8a409ee473
Andrew Heald <andrew@heald.co.uk>
2011-03-15 13:10:12 +0000

    SAPPHO-1: Add custom field for linking back to main issue from an approval or subtask.

---
+++ src/main/java/uk/org/sappho/jira/workflow/approvals/ParentCustomField.java
@@ -0,0 +1,28 @@
+package uk.org.sappho.jira.workflow.approvals;
+
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.fields.CustomField;
+
+@SuppressWarnings("unchecked")
+public class ParentCustomField extends CalculatedCFType {
+
+    public Object getSingularObjectFromString(String customFieldObject) throws FieldValidationException {
+
+        return customFieldObject;
+    }
+
+    public String getStringFromSingularObject(Object customFieldObject) {
+
+        return customFieldObject.toString();
+    }
+
+    public Object getValueFromIssue(CustomField field, Issue issue) {
+
+        String parentKey = "";
+        if (issue.isSubTask())
+            parentKey = issue.getParentObject().getKey();
+        return parentKey;
+    }
+}

539e883ea914651469b692384a07a4a96388323f
Andrew Heald <andrew@heald.co.uk>
2011-03-24 19:17:49 +0000

    SAPPHO-1: Simplify the approvals configuration and make service/type optional for when only approvers are needed, ie. in a subtask.

--- src/main/java/uk/org/sappho/jira/workflow/approvals/ServiceTypeRegion.java
+++ src/main/java/uk/org/sappho/jira/workflow/approvals/ServiceTypeRegion.java
@@ -7,8 +7,6 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.log4j.Logger;
-
 import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.customfields.option.Option;
@@ -19,57 +17,57 @@ import com.opensymphony.workflow.WorkflowException;
 
 public class ServiceTypeRegion implements ApprovalsKey {
 
-    private String project;
-    private String service;
-    private String type;
-    private String region;
+    private String project = null;
+    private String service = null;
+    private String type = null;
+    private String region = null;
     private List<String> requiredApprovalsTypes = null;
     private final Map<String, List<String>> approvers = new HashMap<String, List<String>>();
     private static final Pattern tableRegex = Pattern.compile("^ *\\| +([^\\|]+?) +(\\|.*)$");
     private final ComponentManager componentManager = ComponentManager.getInstance();
-    private static final Logger log = Logger.getLogger(ServiceTypeRegion.class);
 
     public void init(Issue issue) throws WorkflowException {
 
-        // Get project
-        project = issue.getProjectObject().getKey();
-
-        // Get service and type from custom field
+        // Check service / type custom field has been configured
         CustomField serviceTypeField =
                 componentManager.getCustomFieldManager().getCustomFieldObjectByName("Service / Type");
         if (serviceTypeField == null)
-            throw new WorkflowException("Service/Type custom field is not configured for this issue!");
-        Object serviceAndTypeObj = issue.getCustomFieldValue(serviceTypeField);
-        CustomFieldParams serviceAndType = null;
-        if (serviceAndTypeObj != null && serviceAndTypeObj instanceof CustomFieldParams)
-            serviceAndType = (CustomFieldParams) serviceAndTypeObj;
-        if (serviceAndType == null)
-            throw new WorkflowException("Invalid Service/Type field value!");
-        Object serviceObj = serviceAndType.getFirstValueForNullKey();
-        Object typeObj = serviceAndType.getFirstValueForKey("1");
-        service = null;
-        type = null;
-        if (serviceObj != null && serviceObj instanceof Option)
-            service = ((Option) serviceObj).getValue();
-        if (typeObj != null && typeObj instanceof Option)
-            type = ((Option) typeObj).getValue();
-        if (service == null || type == null)
-            throw new WorkflowException("Invalid Service/Type field value!");
-        if (service.length() < 1)
-            service = "None";
-        if (type.length() < 1)
-            type = "None";
+            throw new WorkflowException("Service/Type custom field is not configured!");
 
-        // Get region from reporting user
-        region = issue.getReporter().getPropertySet().getString("region");
-        if (region == null || region.length() < 1)
-            region = "All";
+        // Get service / type from custom field
+        Object serviceAndTypeObj = issue.getCustomFieldValue(serviceTypeField);
+        if (serviceAndTypeObj != null && serviceAndTypeObj instanceof CustomFieldParams) {
+            // Get service and type values which are the main key
+            CustomFieldParams serviceAndType = (CustomFieldParams) serviceAndTypeObj;
+            Object serviceObj = serviceAndType.getFirstValueForNullKey();
+            Object typeObj = serviceAndType.getFirstValueForKey("1");
+            if (serviceObj != null && serviceObj instanceof Option)
+                service = ((Option) serviceObj).getValue();
+            if (typeObj != null && typeObj instanceof Option)
+                type = ((Option) typeObj).getValue();
+            if (service == null || type == null)
+                throw new WorkflowException("Invalid Service/Type field value!");
+            // Get region from reporting user to add to key
+            region = issue.getReporter().getPropertySet().getString("region");
+            if (region == null || region.length() < 1)
+                region = "All";
+        }
+
+        // Get this issue's project key
+        project = issue.getProjectObject().getKey();
 
         // Get approvals configuration page from wiki
         ApprovalsConfiguration config = ApprovalsConfiguration.getInstance();
         String configPage = config.getWikiPage(project, "wiki.approvals.config");
 
-        // Find corresponding approvals
+        // Get all valid issue type names to ensure subtasks can be raised properly
+        Iterable<IssueType> allIssueTypes =
+                componentManager.getConstantsManager().getAllIssueTypeObjects();
+        List<String> allIssueTypeNames = new ArrayList<String>();
+        for (IssueType issueType : allIssueTypes)
+            allIssueTypeNames.add(issueType.getName());
+
+        // Find corresponding approvals by parsing the wiki page
         Map<String, List<String>> possibleApprovals = new HashMap<String, List<String>>();
         for (String configLine : configPage.split("\n")) {
             Matcher matcher = tableRegex.matcher(configLine);
@@ -77,17 +75,23 @@ public class ServiceTypeRegion implements ApprovalsKey {
                 String firstColumn = matcher.group(1);
                 if (config.isIssueType(project, ApprovalsConfiguration.allApprovalsIssueTypes, firstColumn)) {
                     // Handle personnel list for approval type
+                    if (!allIssueTypeNames.contains(firstColumn))
+                        throw new WorkflowException("Approval type " + firstColumn + " is not a valid issue type!");
                     List<String> approversList = new ArrayList<String>();
                     boolean collecting = true;
                     while (collecting) {
                         matcher = tableRegex.matcher(matcher.group(2));
                         collecting = matcher.matches();
-                        if (collecting)
-                            approversList.add(matcher.group(1).toLowerCase());
+                        if (collecting) {
+                            String username = matcher.group(1).trim().toLowerCase();
+                            if (username.length() > 0)
+                                approversList.add(username);
+                        }
                     }
-                    if (approversList.size() > 0)
-                        approvers.put(firstColumn, approversList);
-                } else if (firstColumn.equals(service)) {
+                    if (approversList.size() < 1)
+                        throw new WorkflowException("Approval type " + firstColumn + " has no approvers!");
+                    approvers.put(firstColumn, approversList);
+                } else if (service != null && firstColumn.equals(service)) {
                     matcher = tableRegex.matcher(matcher.group(2));
                     if (matcher.matches() && matcher.group(1).equals(type)) {
                         // Handle service/type configurations for all region combinations
@@ -115,34 +119,14 @@ public class ServiceTypeRegion implements ApprovalsKey {
         }
 
         // Get approvals for specific region, falling back on All if not configured or available
-        requiredApprovalsTypes = possibleApprovals.get(region);
-        if (requiredApprovalsTypes == null)
-            requiredApprovalsTypes = possibleApprovals.get("All");
-
-        // Break the workflow if nothing configured
-        if (requiredApprovalsTypes == null)
-            throw new WorkflowException("No approvals have been configured for " + project + "/" + service + "/" + type
-                    + "/" + region + "! Select a valid service/type combination.");
-
-        // Get all valid issue type names to ensure subtasks can be raised properly
-        Iterable<IssueType> allIssueTypes =
-                componentManager.getConstantsManager().getAllIssueTypeObjects();
-        List<String> allIssueTypeNames = new ArrayList<String>();
-        for (IssueType issueType : allIssueTypes)
-            allIssueTypeNames.add(issueType.getName());
-
-        // Dump configuration to logs and check approvers
-        log.warn("Approval of " + project + "/" + service + "/" + type + "/" + region + ":");
-        for (String requiredApprovalType : requiredApprovalsTypes) {
-            log.warn("  Requires " + requiredApprovalType);
-            if (approvers.get(requiredApprovalType).size() < 1)
-                throw new WorkflowException("Approval type " + requiredApprovalType
-                        + " has no configured approvers! Select a valid service/type combination.");
-            for (String approver : approvers.get(requiredApprovalType))
-                log.warn("    By " + approver);
-            if (!allIssueTypeNames.contains(requiredApprovalType))
-                throw new WorkflowException("Approval type " + requiredApprovalType
-                        + " is not a valid issue type! Select a valid service/type combination.");
+        // but only do this if the issue has a service and type
+        if (service != null) {
+            requiredApprovalsTypes = possibleApprovals.get(region);
+            if (requiredApprovalsTypes == null)
+                requiredApprovalsTypes = possibleApprovals.get("All");
+            if (requiredApprovalsTypes == null)
+                throw new WorkflowException("No approvals have been configured for " + project + "/" + service + "/"
+                        + type + "/" + region + "! Select a valid service/type combination.");
         }
     }
 

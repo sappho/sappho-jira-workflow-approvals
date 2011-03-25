fe856a27cac9dd81e9e13ff61b1ae4beced79637
Andrew Heald <andrew@heald.co.uk>
2011-03-25 16:25:09 +0000

    SAPPHO-1: A bit more logging.

--- src/main/java/uk/org/sappho/jira/workflow/approvals/AssignToApproverAction.java
+++ src/main/java/uk/org/sappho/jira/workflow/approvals/AssignToApproverAction.java
@@ -3,6 +3,8 @@ package uk.org.sappho.jira.workflow.approvals;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.issue.MutableIssue;
 import com.atlassian.jira.workflow.function.issue.AbstractJiraFunctionProvider;
@@ -11,6 +13,8 @@ import com.opensymphony.user.User;
 import com.opensymphony.workflow.WorkflowException;
 
 public class AssignToApproverAction extends AbstractJiraFunctionProvider {
+
+    private static final Logger log = Logger.getLogger(AssignToApproverAction.class);
 
     @SuppressWarnings("unchecked")
     public void execute(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
@@ -27,6 +31,8 @@ public class AssignToApproverAction extends AbstractJiraFunctionProvider {
         User assignee = ComponentManager.getInstance().getUserUtil().getUser(assigneeName);
         if (assignee == null)
             throw new WorkflowException("Configured assignee " + assigneeName + " is invalid!");
+        log.warn("Assigning " + issue.getKey() + " to " + assignee.getFullName()
+                + " because they are the lead approver");
         issue.setAssignee(assignee);
         issue.store();
     }

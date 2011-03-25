02108d07f1053a0433780616b2453bc4459b1e2a
Andrew Heald <andrew@heald.co.uk>
2011-03-25 16:24:44 +0000

    SAPPHO-1: Add post function that properly assigns an issue to the person that raises it.

---
+++ src/main/java/uk/org/sappho/jira/workflow/approvals/AssignToRaiserAction.java
@@ -0,0 +1,27 @@
+package uk.org.sappho.jira.workflow.approvals;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.atlassian.jira.ComponentManager;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.workflow.function.issue.AbstractJiraFunctionProvider;
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.user.User;
+import com.opensymphony.workflow.WorkflowException;
+
+public class AssignToRaiserAction extends AbstractJiraFunctionProvider {
+
+    private static final Logger log = Logger.getLogger(AssignToRaiserAction.class);
+
+    @SuppressWarnings("unchecked")
+    public void execute(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
+
+        User assignee = ComponentManager.getInstance().getJiraAuthenticationContext().getUser();
+        MutableIssue issue = (MutableIssue) transientVars.get("issue");
+        log.warn("Assigning " + issue.getKey() + " to " + assignee.getFullName() + " because they raised it");
+        issue.setAssignee(assignee);
+        issue.store();
+    }
+}

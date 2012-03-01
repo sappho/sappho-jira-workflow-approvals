a84fff441b8e15978a834dc1fb32fcd8cb4b4281
Andrew Heald <andrew@heald.co.uk>
2012-03-01 21:23:03 +0000

    SAPPHO-1: Implement an approvals reverter. More on this. (reverse-merged from commit 557ce60e92e1e11ea4f768b7b7486c84a9c6e472) SAPPHO-1: Implement an approvals reverter. (reverse-merged from commit 8462d29148d4c5ee0b32348b9fa66fd562e02768)

--- src/main/java/uk/org/sappho/jira/workflow/approvals/CleanupApprovalsAction.java
+++ src/main/java/uk/org/sappho/jira/workflow/approvals/CleanupApprovalsAction.java
@@ -6,11 +6,17 @@ import org.apache.log4j.Logger;
 
 import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.util.ErrorCollection;
+import com.atlassian.jira.util.ImportUtils;
+import com.atlassian.jira.util.JiraUtils;
+import com.atlassian.jira.workflow.WorkflowTransitionUtil;
+import com.atlassian.jira.workflow.WorkflowTransitionUtilImpl;
+import com.atlassian.jira.workflow.function.issue.AbstractJiraFunctionProvider;
 import com.opensymphony.module.propertyset.PropertySet;
 import com.opensymphony.user.User;
 import com.opensymphony.workflow.WorkflowException;
 
-public class CleanupApprovalsAction extends BulkApprovalsAction {
+public class CleanupApprovalsAction extends AbstractJiraFunctionProvider {
 
     private final ComponentManager componentManager = ComponentManager.getInstance();
     private static final Logger log = Logger.getLogger(CleanupApprovalsAction.class);
@@ -25,7 +31,26 @@ public class CleanupApprovalsAction extends BulkApprovalsAction {
 
         for (MutableIssue subTask : mainIssue.getSubTaskObjects()) {
             if (subTask.getStatusObject().getName().equals("Approval Sought")) {
-                transitionIssue(subTask, 51, user);
+                int transitionActionId = 51;
+                log.warn("Running workflow transition action id " + transitionActionId + " on " + subTask.getKey());
+                boolean wasIndexing = ImportUtils.isIndexIssues();
+                ImportUtils.setIndexIssues(true);
+                WorkflowTransitionUtil workflowTransitionUtil = JiraUtils
+                        .loadComponent(WorkflowTransitionUtilImpl.class);
+                workflowTransitionUtil.setIssue(subTask);
+                workflowTransitionUtil.setUsername(user.getName());
+                workflowTransitionUtil.setAction(transitionActionId);
+                ErrorCollection errors = workflowTransitionUtil.validate();
+                if (errors.hasAnyErrors())
+                    throw new WorkflowException("Unable to validate transition " + subTask.getKey()
+                            + "! Caused by "
+                            + errors);
+                errors = workflowTransitionUtil.progress();
+                ImportUtils.setIndexIssues(wasIndexing);
+                if (errors.hasAnyErrors())
+                    throw new WorkflowException("Unable to progress transition " + subTask.getKey()
+                            + "! Caused by "
+                            + errors);
             }
         }
     }
